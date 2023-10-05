5ca5ee6599013bbb21193bfaa2ebe40f55b799ff
Vishnu <144769004+vishnum1603@users.noreply.github.com>
2023-10-05 10:42:29 +0530

    Add files via upload

---
+++ Day-12/src/main/java/com/brocode/vishnu/VishnuApplication.java
@@ -0,0 +1,13 @@
+package com.brocode.vishnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VishnuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VishnuApplication.class, args);
+	}
+
+}
---
+++ Day-12/src/main/java/com/brocode/vishnu/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.brocode.vishnu.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.brocode.vishnu.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+}
