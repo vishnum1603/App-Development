fe2bcc256dbeb20630a2585be12ae6b6123d7d97
Vishnu <144769004+vishnum1603@users.noreply.github.com>
2023-10-09 14:37:33 +0530

    Add files via upload

---
+++ Day-14/src/test/java/com/brocode/vishnu/VishnuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brocode.vishnu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VishnuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
